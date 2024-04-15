85b781af11fea3d672082f1ce419bc8ecbb02ffe
xelox <alex@gorodoro.dev>
2024-04-15 23:26:45 +0300

    renaming of _

--- backend/src/server_state.rs
+++ backend/src/server_state.rs
@@ -5,23 +5,23 @@ use axum::extract::ws::WebSocket;
 use crate::structs::client::Client;
 
 pub struct ServerState {
-    client_sockets: RwLock<HashMap<String, Client>>,
+    clients_map: RwLock<HashMap<String, Client>>,
 }
 
 impl ServerState {
     pub fn new() -> ServerState {
         return ServerState {
-            client_sockets: RwLock::new(HashMap::new()),
+            clients_map: RwLock::new(HashMap::new()),
         };
     }
 
     pub async fn add_client(&self, socket: WebSocket, unique_name: String) {
-        let mut sockets = self.client_sockets.write().await;
-        sockets.insert(unique_name.clone(), Client::new(unique_name, socket));
+        let mut clients_map = self.clients_map.write().await;
+        clients_map.insert(unique_name.clone(), Client::new(unique_name, socket));
     }
 
     pub async fn get_client(&self, unique_name: &String) -> Option<Client> {
-        let sockets = self.client_sockets.read().await;
-        return sockets.get(unique_name).cloned();
+        let clients_map = self.clients_map.read().await;
+        return clients_map.get(unique_name).cloned();
     }
 }
