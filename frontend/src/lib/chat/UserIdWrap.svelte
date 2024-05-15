c8cffc11f96886b996760d363128fe435ff27d2c
xelox <alex@gorodoro.dev>
2024-05-15 18:33:52 +0300

    added untracked files

---
+++ frontend/src/lib/chat/UserIdWrap.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { onDestroy } from "svelte";
+import users from "../../stores/users";
+export let id: string;
+
+let username: string = "Unknown";
+const u = users.subscribe(store => {
+  username = store[id]?.display_name ?? store[id]?.username ?? "Unknown";
+});
+
+onDestroy(u);
+</script>
+
+<span>{username}</span>
+
+<style>
+span{
+  color: var(--blue);
+}
+</style>
