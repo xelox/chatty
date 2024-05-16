0edad0371ecb48a4879bb64c51bef31b829ffa16
xelox <alex@gorodoro.dev>
2024-05-16 19:52:28 +0300

    added load_messages path to the router.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -36,6 +36,7 @@ async fn main() {
     let app = Router::new()
         .route("/ws", get(web_socket_manager::handler))
         .route("/app/*any", get(serve_app::serve_app))
+        .route("/api/load_messages/:channel_id/:ts", get(api::load_messages))
         .route("/api/send_message", post(api::send_message))
         .route("/api/send_friend_request", post(api::send_friend_request))
         .route("/api/friendship/:action", post(api::edit_relation))
