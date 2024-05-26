9c9bb612f017b2de516c1084a8c455e19264b211
xelox <alex@gorodoro.dev>
2024-05-26 13:26:25 +0300

    Improved friend list items UI style

--- frontend/src/lib/left_nav/FriendsSection.svelte
+++ frontend/src/lib/left_nav/FriendsSection.svelte
@@ -8,9 +8,14 @@ import Link from '../components/Link.svelte';
   {#each Object.entries($friend_list) as [relation_id, friend_item]}
     <Link to={`/app/chat/friends/${relation_id}`}>
     <div class='friend_item' class:active_channel={relation_id === $router_state.active_channel}>
-      <span class="left"> <img src={`/media/pfp/${friend_item.id}.png`} title="{friend_item.display_name ?? friend_item.username}" alt=""> </span>
+      <span class="left"> 
+        <img src={`/media/pfp/${friend_item.id}.png`} title="{friend_item.display_name ?? friend_item.username}" alt="">
+      </span>
       <div class="right">
-        <span class="display_name">{friend_item.display_name ?? friend_item.username}</span>
+        <p class="line_one">{friend_item.display_name ?? friend_item.username}</p>
+        {#if friend_item.custom_status}
+          <p class="line_two">{friend_item.custom_status}</p>
+        {/if}
       </div>
     </div>
     </Link>
@@ -18,29 +23,34 @@ import Link from '../components/Link.svelte';
 </main>
 
 <style>
-.display_name {
-  font-size: 1em;
+.line_one {
+  font-size: var(--size-large);
+  color: var(--vividtext);
 }
 .left img {
   border-radius: 100%;
-  width: 36px;
+  width: 46px;
   aspect-ratio: 1/1;
   border: none;
-  margin: 0;
 }
 .active_channel {
   background-color: var(--surface0);
 }
+.left {
+  display: flex;
+  padding: 4px;
+}
 .right {
   display: flex;
   flex-direction: column;
-  justify-content: space-around
+  justify-content: center;
+  flex: 1;
+  text-align: left;
 }
 .friend_item {
-  padding: 4px;
   display: flex;
   flex-direction: row;
-  gap: 4px;
+  gap: 10px;
   border-radius: 4px;
 }
 main {
